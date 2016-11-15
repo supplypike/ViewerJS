23f3980461da994d85cee7e88277400c2b3620c8
Simon Constans <simonc@linux.com>
2016-11-15 16:51:30 +0100

    Add video.js with npm

--- build/build.js
+++ build/build.js
@@ -7,6 +7,7 @@ const shell      = require('shelljs'),
       releaseDir = './release',
       sourceDir  = './src',
       sourcePdf  = './node_modules/pdfjs-dist',
+      sourceVideo  = './node_modules/video.js',
       file       = filename => {
           return path.resolve(sourceDir, filename)
       };
@@ -16,11 +17,14 @@ shell.cp(path.resolve(sourceDir, 'example.local.css'), releaseDir);
 shell.cp(path.resolve(sourcePdf, './build/pdf.js'), releaseDir);
 shell.cp(path.resolve(sourcePdf, './build/pdf.worker.js'), releaseDir);
 shell.cp(path.resolve(sourcePdf, './web/compatibility.js'), releaseDir);
+shell.rm('-rf', releaseDir + '/video-js');
+shell.cp('-R', path.resolve(sourceVideo, './dist/'), releaseDir + '/video-js');
 shell.cp(path.resolve(sourceDir, 'ODFViewerPlugin.js'), releaseDir);
 shell.cp(path.resolve(sourceDir, 'PDFViewerPlugin.js'), releaseDir);
 shell.cp(path.resolve(sourceDir, 'ImageViewerPlugin.js'), releaseDir);
 shell.cp(path.resolve(sourceDir, 'MultimediaViewerPlugin.js'), releaseDir);
 shell.cp(path.resolve(sourceDir, 'UnknownFilePlugin.js'), releaseDir);
+shell.cp(path.resolve(sourceDir, 'video-settings.js'), releaseDir);
 
 shell
     .cat([file('additionals.js'),
